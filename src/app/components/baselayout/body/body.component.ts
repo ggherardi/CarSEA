d81e09afd09da7c07685c1396e829fd01e7f83a4
Gianmattia Gherardi <giamalfred@yahoo.it>
2018-04-30 17:04:15 +0200

    Commit HomePage

--- src/app/components/baselayout/body/body.component.ts
+++ src/app/components/baselayout/body/body.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { AppComponent } from '../../../app.component';
+import { ApiAutocompleteConfig } from '../../shared/apiautocomplete/apiautocomplete.component';
 
 @Component({
   selector: 'app-body',
@@ -8,10 +9,39 @@ import { AppComponent } from '../../../app.component';
   styleUrls: ['./body.component.css?ver=${new Date().getTime()}']
 })
 export class BodyComponent implements OnInit {
+  tripSearch: FormGroup;
+  startTime = 0;
+  endTime = 24;
+  autocompleteConfig: ApiAutocompleteConfig = {
+    apiUrl: 'php/CitiesService.php',
+    apiAction: 'search',
+    formattingFunction: (data) => `${data['id_regione']} - ${data['value']}`,
+    valueFormattingFunction: (data) => `${data.value}`
+  };
+  timePickerConfig = {
+    connect: true,
+    step: 1,
+    start: [this.startTime, this.endTime]
+  };
 
-  constructor(private app: AppComponent) { }
+  constructor(private app: AppComponent, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  buildForm() {
+    const date = new Date();
+    const today = {
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      day: date.getDate()
+    };
+    this.tripSearch = this.formBuilder.group({
+      departureCityPicker: [''],
+      arrivalCityPicker: [''],
+      timePicker: [[0, 24]],
+      datePicker: [today]
+    });
+  }
 }
